13a8041790bcece59b79880b64b913c74a773713
Nicole Fernanda Miranda de Arruda <nicole.arruda@in.cassems.com.br>
2022-07-04 12:36:01 -0400

    novas alteracoes

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -16,6 +16,7 @@ export class UsuarioController {
 
   @Post()
   public cria(@Body() usuario: Usuario): Usuario {
+    throw new Error('Erro no cadastro de usuário');
     const usuarioCriado = this.usuarioService.cria(usuario);
     return usuarioCriado;
   }
